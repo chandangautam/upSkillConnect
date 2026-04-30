1b25dc0253bea84f86c1fd70507b59928d1be81a
Chandan <chandangautam322@gmail.com>
2026-04-30 17:04:27 +0545

    userEntity

---
+++ backend/src/main/java/com/upSkillConnect/upSkillConnect_api/entity/userEntity.java
@@ -0,0 +1,31 @@
+package com.upSkillConnect.upSkillConnect_api.entity;
+
+public class userEntity {
+    private long id;
+    private String email;
+    private String password;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String email() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
