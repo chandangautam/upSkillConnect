1d7556d66848bceab969cb166d5a8d3cf4e3ad57
Chandan <chandangautam322@gmail.com>
2026-04-30 17:04:05 +0545

    modified code

--- backend/src/main/java/com/upSkillConnect/upSkillConnect_api/controller/registrationController.java
+++ backend/src/main/java/com/upSkillConnect/upSkillConnect_api/controller/registrationController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import entity.userEntity;
+import com.upSkillConnect.upSkillConnect_api.entity.userEntity;
 
 @RestController
 @RequestMapping("/registration")
