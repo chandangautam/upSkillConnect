9aa52100efc042356121fe0c89c211cc80d75196
Chandan <chandangautam322@gmail.com>
2026-04-30 15:14:22 +0545

    first controller with memory as db for upSKillJob Portal System

---
+++ backend/src/main/java/com/upSkillConnect/upSkillConnect_api/controller/registrationController.java
@@ -0,0 +1,37 @@
+package com.upSkillConnect.upSkillConnect_api.controller;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import entity.userEntity;
+
+@RestController
+@RequestMapping("/registration")
+public class registrationController {
+
+    private Map<Long, userEntity> registrationEntries = new HashMap<>();
+
+    @GetMapping("/company")
+    public ArrayList<userEntity> getAll() {
+        return new ArrayList<>(registrationEntries.values());
+    }
+
+    @PostMapping("/company")
+    public String createUserRegistration(@RequestBody userEntity companyRegistration) {
+        registrationEntries.put(companyRegistration.getId(), companyRegistration);
+        return "Your account has been created!";
+    }
+
+    @GetMapping("/id/{userId}")
+    public userEntity getRegistrationById(@PathVariable Long userId) {
+        return registrationEntries.get(userId); 
+    }
+}
