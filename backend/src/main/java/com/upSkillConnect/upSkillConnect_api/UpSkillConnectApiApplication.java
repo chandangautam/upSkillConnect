a302b87ac617cf057d40acd2abbec95a7be166d4
Chandan <chandangautam322@gmail.com>
2026-04-30 17:03:46 +0545

    driver file modified

--- backend/src/main/java/com/upSkillConnect/upSkillConnect_api/UpSkillConnectApiApplication.java
+++ backend/src/main/java/com/upSkillConnect/upSkillConnect_api/UpSkillConnectApiApplication.java
@@ -2,11 +2,8 @@ package com.upSkillConnect.upSkillConnect_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
-
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication
 public class UpSkillConnectApiApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(UpSkillConnectApiApplication.class, args);
 	}
