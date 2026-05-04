33e189f2da0408e1e3bbced5c693fbfa63eb3163
Chandan <chandangautam322@gmail.com>
2026-05-04 23:55:10 +0545

    add methods to UserRepository for email existence check and role-based user retrieval

--- backend/src/main/java/com/upSkillConnect/upSkillConnect_api/repository/UserRepository.java
+++ backend/src/main/java/com/upSkillConnect/upSkillConnect_api/repository/UserRepository.java
@@ -1,9 +1,14 @@
 package com.upSkillConnect.upSkillConnect_api.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.upSkillConnect.upSkillConnect_api.entity.User;
+import com.upSkillConnect.upSkillConnect_api.entity.enums.Role;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    boolean existsByEmail(String email);
 
+    List<User> findByRole(Role role);
 }
