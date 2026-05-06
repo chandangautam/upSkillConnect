70ad59955be69c1968abdd1ae3e4259d5ecbb508
Chandan <chandangautam322@gmail.com>
2026-05-06 23:39:02 +0545

    add RoleMismatchException class for handling role mismatch errors

---
+++ backend/src/main/java/com/upSkillConnect/upSkillConnect_api/exception/RoleMismatchException.java
@@ -0,0 +1,8 @@
+package com.upSkillConnect.upSkillConnect_api.exception;
+
+public class RoleMismatchException extends RuntimeException {
+
+    public RoleMismatchException(String message) {
+        super(message);
+    }
+}
