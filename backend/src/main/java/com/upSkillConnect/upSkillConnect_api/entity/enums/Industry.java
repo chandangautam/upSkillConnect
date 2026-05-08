cb34b8bd12dad5aac3a72ecaa4286eef9569c899
Chandan <chandangautam322@gmail.com>
2026-05-08 23:40:01 +0545

    add Industry enum to represent various industry sectors

---
+++ backend/src/main/java/com/upSkillConnect/upSkillConnect_api/entity/enums/Industry.java
@@ -0,0 +1,12 @@
+package com.upSkillConnect.upSkillConnect_api.entity.enums;
+
+public enum Industry {
+    SOFTWARE,
+    HEALTH,
+    TECH,
+    FINANCE,
+    MARKETING,
+    CONSTRUCTION,
+    TRAVEL,
+    RETAIL
+}
