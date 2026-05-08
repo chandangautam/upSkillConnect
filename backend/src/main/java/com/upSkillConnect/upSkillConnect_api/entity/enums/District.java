2aa4f4c439d4322e9c6afc219866ec340db69a5b
Chandan <chandangautam322@gmail.com>
2026-05-08 23:39:45 +0545

    add District enum to represent geographical districts

---
+++ backend/src/main/java/com/upSkillConnect/upSkillConnect_api/entity/enums/District.java
@@ -0,0 +1,11 @@
+package com.upSkillConnect.upSkillConnect_api.entity.enums;
+
+public enum District {
+    KATHMANDU,
+    LALITPUR,
+    BHAKTAPUR,
+    KASKI,
+    CHITWAN,
+    BUTWAL,
+    BIRATNAGAR
+}
