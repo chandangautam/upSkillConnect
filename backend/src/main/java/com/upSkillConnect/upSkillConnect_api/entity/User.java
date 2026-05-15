f1caa8d34e873389a59fcc8a380f1d5529db0dab
Chandan <chandangautam322@gmail.com>
2026-05-15 23:28:52 +0545

    refactor User entity to use Long type for id and update getter method

--- backend/src/main/java/com/upSkillConnect/upSkillConnect_api/entity/User.java
+++ backend/src/main/java/com/upSkillConnect/upSkillConnect_api/entity/User.java
@@ -4,12 +4,14 @@ import java.time.LocalDateTime;
 
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import com.upSkillConnect.upSkillConnect_api.entity.enums.Role;
 import com.upSkillConnect.upSkillConnect_api.entity.enums.Status;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
@@ -19,11 +21,12 @@ import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "users")
+@EntityListeners(AuditingEntityListener.class)
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    private Long id;
 
     @Column(name = "email", unique = true, nullable = false)
     private String email;
@@ -62,7 +65,7 @@ public class User {
         this.rejectionReason = rejectionReason;
     }
 
-    public long getId() {
+    public Long getId() {
         return id;
     }
 
