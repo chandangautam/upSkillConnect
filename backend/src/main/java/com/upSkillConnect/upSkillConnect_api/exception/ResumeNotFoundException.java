ddd2a25dda26d7c3c443e0f31e8cca21a1c3ff5d
Chandan <chandangautam322@gmail.com>
2026-05-15 23:31:41 +0545

    add ResumeNotFoundException class for handling resume not found errors

---
+++ backend/src/main/java/com/upSkillConnect/upSkillConnect_api/exception/ResumeNotFoundException.java
@@ -0,0 +1,8 @@
+package com.upSkillConnect.upSkillConnect_api.exception;
+
+public class ResumeNotFoundException extends RuntimeException {
+
+    public ResumeNotFoundException(String message) {
+        super(message);
+    }
+}
