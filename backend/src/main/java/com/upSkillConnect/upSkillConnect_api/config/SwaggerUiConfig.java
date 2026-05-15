2cb2c667a3ecfd16c17f3d43827abc39efde1b93
Chandan <chandangautam322@gmail.com>
2026-05-15 23:28:08 +0545

    add SwaggerUiConfig to configure Swagger documentation for the API

---
+++ backend/src/main/java/com/upSkillConnect/upSkillConnect_api/config/SwaggerUiConfig.java
@@ -0,0 +1,18 @@
+package com.upSkillConnect.upSkillConnect_api.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.OpenAPI;
+
+@Configuration
+public class SwaggerUiConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("UpSKill_API")
+                        .version("1.0")
+                        .description("Spring Boot Swagger Documentation"));
+    }
+}
