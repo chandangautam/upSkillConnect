a5a90d51495cc5b7c19b1bc31a02cc9870764813
Chandan <chandangautam322@gmail.com>
2026-06-23 16:45:54 +0545

    add CompanyNameAlreadyExistsException for handling duplicate company names

---
+++ backend/src/main/java/com/upSkillConnect/upSkillConnect_api/exception/CompanyNameAlreadyExistsException.java
@@ -0,0 +1,7 @@
+package com.upSkillConnect.upSkillConnect_api.exception;
+
+public class CompanyNameAlreadyExistsException extends RuntimeException {
+    public CompanyNameAlreadyExistsException(String message) {
+        super(message);
+    }
+}
