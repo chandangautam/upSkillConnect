fee70fd53f95449a42441cb4116085b51b47b8ff
Chandan <chandangautam322@gmail.com>
2026-06-23 16:44:55 +0545

    refactor ApiResponseDTO to include data and errors fields

--- backend/src/main/java/com/upSkillConnect/upSkillConnect_api/common/ApiResponseDTO.java
+++ backend/src/main/java/com/upSkillConnect/upSkillConnect_api/common/ApiResponseDTO.java
@@ -1,17 +1,26 @@
 package com.upSkillConnect.upSkillConnect_api.common;
 
+import java.util.Map;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class ApiResponseDTO<T> {
 
     private String message;
-    boolean success;
+    private boolean success;
+    private T data;
+    Map<String, String> errors;
 
     public ApiResponseDTO() {
 
     }
 
-    public ApiResponseDTO(String message, boolean success) {
+    public ApiResponseDTO(String message, boolean success, T data, Map<String, String> errors) {
         this.message = message;
         this.success = success;
+        this.data = data;
+        this.errors = errors;
     }
 
     public String getMessage() {
@@ -20,5 +29,13 @@ public class ApiResponseDTO<T> {
 
     public boolean getSuccess() {
         return success;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public Map<String, String> getErrors() {
+        return errors;
     }
 }
