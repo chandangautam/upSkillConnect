22bf08d3a9173c96e2894b0e8166ca102d3e2485
Chandan <chandangautam322@gmail.com>
2026-06-23 16:41:52 +0545

    add CompanySize enum for categorizing company sizes

---
+++ backend/src/main/java/com/upSkillConnect/upSkillConnect_api/entity/enums/CompanySize.java
@@ -0,0 +1,9 @@
+package com.upSkillConnect.upSkillConnect_api.entity.enums;
+
+public enum CompanySize {
+    ONE_TO_TEN,
+    ELEVEN_TO_FIFTY,
+    FIFTY_ONE_TO_TWO_HUNDRED,
+    TWO_HUNDRED_ONE_TO_FIVE_HUNDRED,
+    FIVE_HUNDRED_PLUS
+}
